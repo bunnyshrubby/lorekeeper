d5b905465ccfc0ef8ce315a9c1c36e0c079d7d38
amiyourhiro <dimatreukhov@gmail.com>
2023-10-02 10:57:58 +0300

    fix?

--- app/Services/Item/ChoiceboxService.php
+++ app/Services/Item/ChoiceboxService.php
@@ -10,6 +10,7 @@ use App\Models\Item\Item;
 use App\Models\Currency\Currency;
 use App\Models\Loot\LootTable;
 use App\Models\Raffle\Raffle;
+use App\Models\Award\Award;
 
 class ChoiceboxService extends Service
 {
