e9eb0b068efc58cb779cf190410f7dab950d8232
amiyourhiro <dimatreukhov@gmail.com>
2023-10-02 16:33:07 +0300

    bruuuh

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -41,6 +41,7 @@ class ShopController extends Controller
             $shops = count($shopcategories) ? Shop::where('is_active', 1)->orderByRaw('FIELD(shop_category_id,'.implode(',', $shopcategories->pluck('id')->toArray()).')')->orderBy('name')->get()->groupBy('shop_category_id') : Shop::where('is_active', 1)->orderBy('name')->get()->groupBy('shop_category_id');
         
         return view('shops.index', [
+            'shops' => Shop::where('is_active', 1)->orderBy('sort', 'DESC')->get(),
             'shopcategories' => $shopcategories->keyBy('id'),
             'shops' => $shops,
             ]);
@@ -124,6 +125,8 @@ class ShopController extends Controller
 
         return view('shops.shop', [
             'shop' => $shop,
+            'stocks' => $stocks,
+            'shops' => Shop::where('is_active', 1)->orderBy('sort', 'DESC')->get(),
             'categories' => $categories->keyBy('id'),
             'items' => $items,
             'shopcategories' => $shopcategories->keyBy('id'),
