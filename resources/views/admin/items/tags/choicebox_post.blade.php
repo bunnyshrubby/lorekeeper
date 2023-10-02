9a131e278b56a72acf58d5d33ed267464852c9a6
amiyourhiro <dimatreukhov@gmail.com>
2023-10-02 10:47:16 +0300

    change

--- resources/views/admin/items/tags/choicebox_post.blade.php
+++ resources/views/admin/items/tags/choicebox_post.blade.php
@@ -1,1 +1,1 @@
-@include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'showLootTables' => true, 'showRaffles' => true, 'awards' => $awards])
+@include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'awards' => $awards, 'showLootTables' => true, 'showRaffles' => true])
