36189e777def9bc74d5acc4b0cbd483883aa2470
amiyourhiro <dimatreukhov@gmail.com>
2023-10-06 11:29:38 +0300

    no message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -78,7 +78,7 @@
 
 <div class="card-deck mb-4 profile-assets" style="clear:both;">
     <div class="card profile-currencies profile-assets-card">
-        <div class="card-body text-center d-flex">
+        <div class="card-body text-center d-flex align-items-center">
             <h5 class="card-title">Bank</h5>
             <div class="profile-assets-content">
                 @foreach($user->getCurrencies(false) as $currency)
