8036b8686484567d99975551c8db7b36817ca515
amiyourhiro <dimatreukhov@gmail.com>
2023-10-15 15:40:52 +0300

    no message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -59,23 +59,23 @@
     </div>
 </div>
 
+ @if($user->is_deactivated)
+    <div class="alert alert-info text-center">
+        <h1>{!! $user->displayName !!}</h1>
+        <p>This account is currently deactivated, be it by staff or the user's own action. All information herein is hidden until the account is reactivated.</p>
+        @if(Auth::check() && Auth::user()->isStaff)
+        <p class="mb-0">As you are staff, you can see the profile contents below and the sidebar contents.</p>
+	</div>
+	    @endif
+@endif
+
 @if(isset($user->profile->parsed_text))
-    <div class="card mb-3" style="clear:both;">
+    <div class="card mb-4" style="clear:both;">
         <div class="card-body">
             {!! $user->profile->parsed_text !!}
         </div>
     </div>
 @endif
-
-@if($user->is_deactivated)
-    	   <div class="alert alert-info text-center">
-            <h1>{!! $user->displayName !!}</h1>
-            <p>This account is currently deactivated, be it by staff or the user's own action. All information herein is hidden until the account is reactivated.</p>
-        @if(Auth::check() && Auth::user()->isStaff)
-            <p class="mb-0">As you are staff, you can see the profile contents below and the sidebar contents.</p>
-	       </div>
-	    @endif
-    @endif
 
 <div class="card-deck mb-4 profile-assets" style="clear:both;">
     <div class="card profile-currencies profile-assets-card">
