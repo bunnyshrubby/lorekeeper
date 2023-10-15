71586f447825ceca5dcbdaafea0727391243e2ae
amiyourhiro <dimatreukhov@gmail.com>
2023-10-15 17:06:39 +0300

    no message

--- resources/views/world/index.blade.php
+++ resources/views/world/index.blade.php
@@ -16,9 +16,9 @@
             <ul class="list-group list-group-flush">
                 <li class="list-group-item"><a href="{{ url('world/'.__('lorekeeper.specieses')) }}">{{ ucfirst(__('lorekeeper.specieses')) }}</a></li>
 				<li class="list-group-item"><a href="{{ url('world/'.__('lorekeeper.subtypes')) }}">{{ ucfirst(__('lorekeeper.subtypes')) }}</a></li>
+                <li class="list-group-item"><a href="{{ url('world/traits') }}">All Traits</a></li>
                 <li class="list-group-item"><a href="{{ url('world/rarities') }}">Rarities</a></li>
                 <li class="list-group-item"><a href="{{ url('world/trait-categories') }}">Trait Categories</a></li>
-                <li class="list-group-item"><a href="{{ url('world/traits') }}">All Traits</a></li>
                 <li class="list-group-item"><a href="{{ url('world/character-categories') }}">Character Categories</a></li>
             </ul>
         </div>
@@ -30,11 +30,11 @@
                 <h5 class="card-title">Items & {{ ucfirst(__('awards.awards')) }}</h5>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item"><a href="{{ url('world/item-categories') }}">Item Categories</a></li>
-                <li class="list-group-item"><a href="{{ url('world/items') }}">All Items</a></li>
-                <li class="list-group-item"><a href="{{ url('world/'.__('awards.award').'-categories') }}">{{ ucfirst(__('awards.award')) }} Categories</a></li>
-                <li class="list-group-item"><a href="{{ url('world/'.__('awards.awards')) }}">All {{ ucfirst(__('awards.awards')) }}</a></li>
                 <li class="list-group-item"><a href="{{ url('world/currencies') }}">Currencies</a></li>
+                <li class="list-group-item"><a href="{{ url('world/items') }}">All Items</a></li>
+                <li class="list-group-item"><a href="{{ url('world/'.__('awards.awards')) }}">All {{ ucfirst(__('awards.awards')) }}</a></li>
+                <li class="list-group-item"><a href="{{ url('world/item-categories') }}">Item Categories</a></li>
+                <li class="list-group-item"><a href="{{ url('world/'.__('awards.award').'-categories') }}">{{ ucfirst(__('awards.award')) }} Categories</a></li>
             </ul>
         </div>
     </div>
