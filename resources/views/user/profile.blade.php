40a0a072c6b79495bb778cd4c0b821f4622db563
amiyourhiro <dimatreukhov@gmail.com>
2023-10-15 15:41:20 +0300

    no message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -70,14 +70,14 @@
 @endif
 
 @if(isset($user->profile->parsed_text))
-    <div class="card mb-4" style="clear:both;">
+    <div class="card mb-3" style="clear:both;">
         <div class="card-body">
             {!! $user->profile->parsed_text !!}
         </div>
     </div>
 @endif
 
-<div class="card-deck mb-4 profile-assets" style="clear:both;">
+<div class="card-deck mb-3 profile-assets" style="clear:both;">
     <div class="card profile-currencies profile-assets-card">
         <div class="card-body text-center">
             <h5 class="card-title">Bank</h5>
