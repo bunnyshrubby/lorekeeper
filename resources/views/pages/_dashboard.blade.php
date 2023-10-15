254f0dbc19f5ff9a43bf6f4ca25f0721d2d53bcc
amiyourhiro <dimatreukhov@gmail.com>
2023-10-15 17:00:44 +0300

    test

--- resources/views/pages/_dashboard.blade.php
+++ resources/views/pages/_dashboard.blade.php
@@ -1,12 +1,10 @@
-@include('widgets._online_count')
-
-
 <h1>Welcome, {!! Auth::user()->displayName !!}!</h1>
 <div class="card mb-4 timestamp">
     <div class="card-body">
         <i class="far fa-clock"></i> {!! format_date(Carbon\Carbon::now()) !!}
     </div>
 </div>
+@include('widgets._online_count')
 
 <div class="row justify-content-center">
     <div class="col-md-6">
