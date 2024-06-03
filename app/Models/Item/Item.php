5222c36e111dc7004406ec62506d26272a05ecda
amiyourhiro <dimatreukhov@gmail.com>
2024-06-03 12:57:28 +0300

    no message

--- app/Models/Item/Item.php
+++ app/Models/Item/Item.php
@@ -123,7 +123,7 @@ class Item extends Model
     public function scopeSortCategory($query)
     {
         $ids = ItemCategory::orderBy('sort', 'DESC')->pluck('id')->toArray();
-        return count($ids) ? $query->orderByRaw(DB::raw('FIELD(id, '.implode(',', $ids).')')) : $query;
+        return count($ids) ? $query->orderByRaw(DB::raw('FIELD(item_category_id, '.implode(',', $ids).')')) : $query;
     }
 
     /**
