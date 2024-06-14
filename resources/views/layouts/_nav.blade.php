2ba7e04cb023202a2fe0d87afae074274104ceab
amiyourhiro <dimatreukhov@gmail.com>
2024-06-14 21:10:54 +0300

    nav changes

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -143,6 +143,23 @@
                         <a class="dropdown-item" href="{{ url('world') }}">
                             Encyclopedia
                         </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="{{ url('world/subtypes') }}">
+                            Subtypes
+                        </a>
+                        <a class="dropdown-item" href="{{ url('world/traits') }}">
+                            Traits
+                        </a>
+                        <a class="dropdown-item" href="{{ url('world/items') }}">
+                            Items
+                        </a>
+                        <a class="dropdown-item" href="{{ url('shops') }}">
+                            Shops
+                        </a>
+                        <a class="dropdown-item" href="{{ url('world/awards') }}">
+                            Awards
+                        </a>
+                        <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="{{ url('world/info') }}">
                             World
                         </a>
